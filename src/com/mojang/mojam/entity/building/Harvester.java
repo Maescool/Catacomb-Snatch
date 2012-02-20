c25d59fa82f58c969ac635c859f33eb0d6b1d5f0
Pieter Maes <maescool@gmail.com>
2012-02-20 21:57:54 +0100

    fixed formatting -DOH-

--- src/com/mojang/mojam/entity/building/Harvester.java
+++ src/com/mojang/mojam/entity/building/Harvester.java
@@ -15,178 +15,180 @@ import java.util.Random;
 
 public class Harvester extends Building implements LootCollector {
 
-	private int capacity = 5000;
-	private int money = 0;
-	private int time = 0;
-	private int harvestingTicks = 20;
-	private boolean isHarvesting = false;
-	private boolean isEmptying = false;
-	private Player emptyingPlayer = null;
-	private int emptyingSpeed = 50;
-	private double radius;
-	private int[] upgradeRadius = new int[] { (int) (1.5 * Tile.WIDTH),
-			2 * Tile.WIDTH, (int) (2.5 * Tile.WIDTH) };
-	private int[] upgradeCapacities = new int[] { 1500, 2500, 3500 };
+    private int capacity = 5000;
+    private int money = 0;
+    private int time = 0;
+    private int harvestingTicks = 20;
+    private boolean isHarvesting = false;
+    private boolean isEmptying = false;
+    private Player emptyingPlayer = null;
+    private int emptyingSpeed = 50;
+    private double radius;
+    private int[] upgradeRadius = new int[] { (int) (1.5 * Tile.WIDTH),
+            2 * Tile.WIDTH, (int) (2.5 * Tile.WIDTH) };
+    private int[] upgradeCapacities = new int[] { 1500, 2500, 3500 };
 
-	public Harvester(double x, double y, int team) {
-		super(x, y, team);
-		setStartHealth(20);
-		freezeTime = 10;
-		makeUpgradeableWithCosts(new int[] { 500, 1000, 5000 });
-	}
+    public Harvester(double x, double y, int team) {
+        super(x, y, team);
+        setStartHealth(20);
+        freezeTime = 10;
+        makeUpgradeableWithCosts(new int[] { 500, 1000, 5000 });
+    }
 
-	public void notifySucking() {
-		harvestingTicks = 30;
-	}
+    public void notifySucking() {
+        harvestingTicks = 30;
+    }
 
-	public boolean isAllowedToCancel() {
-		return !isEmptying;
-	}
+    public boolean isAllowedToCancel() {
+        return !isEmptying;
+    }
 
-	public void tick() {
-		super.tick();
-		if (--freezeTime > 0) {
-			return;
-		}
+    public void tick() {
+        super.tick();
+        if (--freezeTime > 0) {
+            return;
+        }
 
-		if (isEmptying && (time % 3 == 0)) {
-			if (money <= 0) {
-				isEmptying = false;
-			} else {
-				Loot l = new Loot(pos.x, pos.y, 0, 0, 1);
-				l.fake = true;
-				l.life = 20;
-				l.forceTake(emptyingPlayer);
-				int toAdd = Math.min(emptyingSpeed, money);
-				money -= toAdd;
-				emptyingPlayer.addScore(toAdd);
-				level.addEntity(l);
-			}
-		}
+        if (isEmptying && (time % 3 == 0)) {
+            if (money <= 0) {
+                isEmptying = false;
+            } else {
+                Loot l = new Loot(pos.x, pos.y, 0, 0, 1);
+                l.fake = true;
+                l.life = 20;
+                l.forceTake(emptyingPlayer);
+                int toAdd = Math.min(emptyingSpeed, money);
+                money -= toAdd;
+                emptyingPlayer.addScore(toAdd);
+                level.addEntity(l);
+            }
+        }
 
-		++time;
-		--harvestingTicks;
-		isHarvesting = (harvestingTicks >= 0);
+        ++time;
+        --harvestingTicks;
+        isHarvesting = (harvestingTicks >= 0);
 
-		Random random = TurnSynchronizer.synchedRandom;
-		if (isHarvesting) {
-			if (random.nextDouble() < 0.050f) {
-				level.addEntity(new SmokeAnimation(pos.x - 6
-						+ random.nextInt(8) - random.nextInt(8), pos.y - 16,
-						Art.fxSteam12, 30));
-			}
-		} else {
-			if (random.nextDouble() < 0.002f) {
-				level.addEntity(new SmokeAnimation(pos.x - 6
-						+ random.nextInt(8) - random.nextInt(8), pos.y - 16,
-						Art.fxSteam12, 30));
-			}
-		}
-		if (health == 0) {
-			dropAllMoney();
-		}
-	}
+        Random random = TurnSynchronizer.synchedRandom;
+        if (isHarvesting) {
+            if (random.nextDouble() < 0.050f) {
+                level.addEntity(new SmokeAnimation(pos.x - 6
+                        + random.nextInt(8) - random.nextInt(8), pos.y - 16,
+                        Art.fxSteam12, 30));
+            }
+        } else {
+            if (random.nextDouble() < 0.002f) {
+                level.addEntity(new SmokeAnimation(pos.x - 6
+                        + random.nextInt(8) - random.nextInt(8), pos.y - 16,
+                        Art.fxSteam12, 30));
+            }
+        }
+        if (health == 0) {
+            dropAllMoney();
+        }
+    }
 
-	@Override
-	public Bitmap getSprite() {
-		int frame = isHarvesting ? (4 + ((time >> 3) % 5)) : (time >> 3) % 4;
-		return Art.harvester[frame][0];
-	}
+    @Override
+    public Bitmap getSprite() {
+        int frame = isHarvesting ? (4 + ((time >> 3) % 5)) : (time >> 3) % 4;
+        return Art.harvester[frame][0];
+    }
 
-	protected void upgradeComplete(int upgradeLevel) {
-		radius = upgradeRadius[upgradeLevel];
-		capacity = upgradeCapacities[upgradeLevel];
-	}
+    protected void upgradeComplete(int upgradeLevel) {
+        radius = upgradeRadius[upgradeLevel];
+        capacity = upgradeCapacities[upgradeLevel];
+    }
 
-	public boolean canTake() {
-		return money < capacity;
-	}
+    public boolean canTake() {
+        return money < capacity;
+    }
 
-	public void render(Screen screen) {
-		Bitmap image = getSprite();
+    public void render(Screen screen) {
+        Bitmap image = getSprite();
 
-		if (hurtTime > 0) {
-			if (hurtTime > 40 - 6 && hurtTime / 2 % 2 == 0) {
-				screen.colorBlit(image, pos.x - image.w / 2, pos.y - image.h
-						+ 8, 0xa0ffffff);
-			} else {
-				if (health < 0) {
-					health = 0;
-				}
-				int col = 180 - health * 180 / maxHealth;
-				if (hurtTime < 10) {
-					col = col * hurtTime / 10;
-				}
-				screen.colorBlit(image, pos.x - image.w / 2, pos.y - image.h
-						+ 8, (col << 24) + 255 * 65536);
-			}
-		} else if (capacity - money < 500) {
-			screen.colorBlit(image, pos.x - image.w / 2, pos.y - image.h + 8,
-					0x77ff7200);
-		} else {
-			screen.blit(image, pos.x - image.w / 2, pos.y - image.h + 8);
-		}
-		renderMarker(screen);
-		if (hurtTime > 0) {
-			if (hurtTime > 40 - 6 && hurtTime / 2 % 2 == 0) {
-				screen.colorBlit(image, pos.x - image.w / 2, pos.y - image.h
-						+ 8, 0xa0ffffff);
-			} else {
-				if (health < 0) {
-					health = 0;
-				}
-				int col = 180 - health * 180 / maxHealth;
-				if (hurtTime < 10) {
-					col = col * hurtTime / 10;
-				}
-				screen.colorBlit(image, pos.x - image.w / 2, pos.y - image.h
-						+ 8, (col << 24) + 255 * 65536);
-			}
-		} else {
-			screen.blit(image, pos.x - image.w / 2, pos.y - image.h + 8);
-		}
-		Font.draw(screen, money + " / " + capacity, (int) (pos.x - 25), (int) (pos.y - 30));
-		Font.draw(screen, health + " / " + maxHealth, (int) (pos.x - 27), (int) (pos.y + 10));
-	}
+        if (hurtTime > 0) {
+            if (hurtTime > 40 - 6 && hurtTime / 2 % 2 == 0) {
+                screen.colorBlit(image, pos.x - image.w / 2, pos.y - image.h
+                        + 8, 0xa0ffffff);
+            } else {
+                if (health < 0) {
+                    health = 0;
+                }
+                int col = 180 - health * 180 / maxHealth;
+                if (hurtTime < 10) {
+                    col = col * hurtTime / 10;
+                }
+                screen.colorBlit(image, pos.x - image.w / 2, pos.y - image.h
+                        + 8, (col << 24) + 255 * 65536);
+            }
+        } else if (capacity - money < 500) {
+            screen.colorBlit(image, pos.x - image.w / 2, pos.y - image.h + 8,
+                    0x77ff7200);
+        } else {
+            screen.blit(image, pos.x - image.w / 2, pos.y - image.h + 8);
+        }
+        renderMarker(screen);
+        if (hurtTime > 0) {
+            if (hurtTime > 40 - 6 && hurtTime / 2 % 2 == 0) {
+                screen.colorBlit(image, pos.x - image.w / 2, pos.y - image.h
+                        + 8, 0xa0ffffff);
+            } else {
+                if (health < 0) {
+                    health = 0;
+                }
+                int col = 180 - health * 180 / maxHealth;
+                if (hurtTime < 10) {
+                    col = col * hurtTime / 10;
+                }
+                screen.colorBlit(image, pos.x - image.w / 2, pos.y - image.h
+                        + 8, (col << 24) + 255 * 65536);
+            }
+        } else {
+            screen.blit(image, pos.x - image.w / 2, pos.y - image.h + 8);
+        }
+        Font.draw(screen, money + " / " + capacity, (int) (pos.x - 25),
+                (int) (pos.y - 30));
+        Font.draw(screen, health + " / " + maxHealth, (int) (pos.x - 27),
+                (int) (pos.y + 10));
+    }
 
-	public void take(Loot loot) {
-		loot.remove();
-		money += loot.getScoreValue();
-		if (money > capacity) {
-			money = capacity;
-		}
-	}
+    public void take(Loot loot) {
+        loot.remove();
+        money += loot.getScoreValue();
+        if (money > capacity) {
+            money = capacity;
+        }
+    }
 
-	public double getSuckPower() {
-		return radius / 60.0;
-	}
+    public double getSuckPower() {
+        return radius / 60.0;
+    }
 
-	public void flash() {
-	}
+    public void flash() {
+    }
 
-	public int getScore() {
-		return money;
-	}
+    public int getScore() {
+        return money;
+    }
 
-	public void dropAllMoney() {
+    public void dropAllMoney() {
 
-		while (money > 0) {
-			double dir = TurnSynchronizer.synchedRandom.nextDouble() * Math.PI
-					* 2;
-			Loot loot = new Loot(pos.x, pos.y, Math.cos(dir), Math.sin(dir),
-					money / 2);
-			level.addEntity(loot);
+        while (money > 0) {
+            double dir = TurnSynchronizer.synchedRandom.nextDouble() * Math.PI
+                    * 2;
+            Loot loot = new Loot(pos.x, pos.y, Math.cos(dir), Math.sin(dir),
+                    money / 2);
+            level.addEntity(loot);
 
-			money -= loot.getScoreValue();
-		}
-		money = 0;
-	}
+            money -= loot.getScoreValue();
+        }
+        money = 0;
+    }
 
-	public void use(Entity user) {
-		super.use(user);
-		isEmptying = true;
-		if (user instanceof Player) {
-			emptyingPlayer = (Player) user;
-		}
-	}
+    public void use(Entity user) {
+        super.use(user);
+        isEmptying = true;
+        if (user instanceof Player) {
+            emptyingPlayer = (Player) user;
+        }
+    }
 }
--- src/com/mojang/mojam/entity/building/Turret.java
+++ src/com/mojang/mojam/entity/building/Turret.java
@@ -10,81 +10,82 @@ import com.mojang.mojam.screen.*;
 
 public class Turret extends Building {
 
-	private int delayTicks = 0;
-	private int delay;
-	private double radius;
-	private double radiusSqr;
+    private int delayTicks = 0;
+    private int delay;
+    private double radius;
+    private double radiusSqr;
 
-	private int[] upgradeRadius = new int[] { 3 * Tile.WIDTH, 5 * Tile.WIDTH,
-			7 * Tile.WIDTH };
-	private int[] upgradeDelay = new int[] { 24, 21, 18 };
+    private int[] upgradeRadius = new int[] { 3 * Tile.WIDTH, 5 * Tile.WIDTH,
+            7 * Tile.WIDTH };
+    private int[] upgradeDelay = new int[] { 24, 21, 18 };
 
-	private int facing = 0;
+    private int facing = 0;
 
-	public Turret(double x, double y, int team) {
-		super(x, y, team);
-		setStartHealth(20);
-		freezeTime = 10;
+    public Turret(double x, double y, int team) {
+        super(x, y, team);
+        setStartHealth(20);
+        freezeTime = 10;
 
-		makeUpgradeableWithCosts(new int[] { 500, 1000, 5000 });
-	}
+        makeUpgradeableWithCosts(new int[] { 500, 1000, 5000 });
+    }
 
-	public void init() {
-	}
+    public void init() {
+    }
 
-	public void tick() {
-		super.tick();
-		if (--freezeTime > 0)
-			return;
-		if (--delayTicks > 0)
-			return;
+    public void tick() {
+        super.tick();
+        if (--freezeTime > 0)
+            return;
+        if (--delayTicks > 0)
+            return;
 
-		Set<Entity> entities = level.getEntities(pos.x - radius,
-				pos.y - radius, pos.x + radius, pos.y + radius);
+        Set<Entity> entities = level.getEntities(pos.x - radius,
+                pos.y - radius, pos.x + radius, pos.y + radius);
 
-		Entity closest = null;
-		double closestDist = 99999999.0f;
-		for (Entity e : entities) {
-			if (!(e instanceof Mob))
-				continue;
-			if ((e instanceof RailDroid))
-				continue;
-			if (!((Mob) e).isNotFriendOf(this))
-				continue;
-			final double dist = e.pos.distSqr(pos);
-			if (dist < radiusSqr && dist < closestDist) {
-				closestDist = dist;
-				closest = e;
-			}
-		}
-		if (closest == null)
-			return;
+        Entity closest = null;
+        double closestDist = 99999999.0f;
+        for (Entity e : entities) {
+            if (!(e instanceof Mob))
+                continue;
+            if ((e instanceof RailDroid))
+                continue;
+            if (!((Mob) e).isNotFriendOf(this))
+                continue;
+            final double dist = e.pos.distSqr(pos);
+            if (dist < radiusSqr && dist < closestDist) {
+                closestDist = dist;
+                closest = e;
+            }
+        }
+        if (closest == null)
+            return;
 
-		double invDist = 1.0 / Math.sqrt(closestDist);
-		double yd = closest.pos.y - pos.y;
-		double xd = closest.pos.x - pos.x;
-		double angle = (Math.atan2(yd, xd) + Math.PI * 1.625);
-		facing = (8 + (int) (angle / Math.PI * 4)) & 7;
-		Bullet bullet = new Bullet(this, xd * invDist, yd * invDist);
-		bullet.pos.y -= 10;
-		level.addEntity(bullet);
-		delayTicks = delay;
-	}
+        double invDist = 1.0 / Math.sqrt(closestDist);
+        double yd = closest.pos.y - pos.y;
+        double xd = closest.pos.x - pos.x;
+        double angle = (Math.atan2(yd, xd) + Math.PI * 1.625);
+        facing = (8 + (int) (angle / Math.PI * 4)) & 7;
+        Bullet bullet = new Bullet(this, xd * invDist, yd * invDist);
+        bullet.pos.y -= 10;
+        level.addEntity(bullet);
+        delayTicks = delay;
+    }
 
-	public void render(Screen screen) {
-		super.render(screen);
-		if (health != 0) {
-			Font.draw(screen, health + " / " + maxHealth, (int) (pos.x - 30), (int) (pos.y + 12));
-		}
-	}
+    public void render(Screen screen) {
+        super.render(screen);
+        if (health != 0) {
+            Font.draw(screen, health + " / " + maxHealth, (int) (pos.x - 30),
+                    (int) (pos.y + 12));
+        }
+    }
 
-	public Bitmap getSprite() {
-		return Art.turret[facing][0];
-	}
+    public Bitmap getSprite() {
+        return Art.turret[facing][0];
+    }
 
-	protected void upgradeComplete(int upgradeLevel) {
-		delay = upgradeDelay[upgradeLevel];
-		radius = upgradeRadius[upgradeLevel];
-		radiusSqr = radius * radius;
-	}
+    protected void upgradeComplete(int upgradeLevel) {
+        delay = upgradeDelay[upgradeLevel];
+        radius = upgradeRadius[upgradeLevel];
+        radiusSqr = radius * radius;
+    }
 }
