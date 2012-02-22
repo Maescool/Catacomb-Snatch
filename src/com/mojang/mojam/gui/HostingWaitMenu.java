d2224c1e6a8a44c96856df8675f6d6e2fcd333f9
cannabeuh <admin@mine-france.fr>
2012-02-22 07:30:19 +0100

    IP LAN/WAN
    Change-Id: I6d1797d6ec61d34db7f215683a7eb5d76f4ef43c

--- src/com/mojang/mojam/gui/HostingWaitMenu.java
+++ src/com/mojang/mojam/gui/HostingWaitMenu.java
@@ -1,30 +1,60 @@
 package com.mojang.mojam.gui;
 
 import java.awt.event.KeyEvent;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.*;
 
 import com.mojang.mojam.screen.Screen;
 
 public class HostingWaitMenu extends GuiMenu {
 
+	public String myIpLAN;
+	public String myIpWAN;
 	public HostingWaitMenu() {
 		super();
-
 		addButton(new Button(TitleMenu.CANCEL_JOIN_ID, "Cancel", 250, 180));
 	}
-
-	@Override
-	public void render(Screen screen) {
-
-		screen.clear(0);
-		Font.draw(screen, "Waiting for client to join...", 100, 100);
+	public void searchIpWAN(){
+		URL whatismyip;
+		try {
+			whatismyip = new URL("http://automation.whatismyip.com/n09230945.asp");
+			BufferedReader in;
+			try {
+				in = new BufferedReader(new InputStreamReader(
+				whatismyip.openStream()));
+				myIpWAN = in.readLine(); 
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+	}
+	public void searchIpLAN(){
 		try {
 			InetAddress thisIp = InetAddress.getLocalHost();
-			Font.draw(screen, "Your IP:" + thisIp.getHostAddress(), 100, 120);
+			myIpLAN = thisIp.getHostAddress();
 		} catch (Exception e) {
 			e.printStackTrace();
-
 		}
+	}
+	@Override
+	public void render(Screen screen) {
+		screen.clear(0);
+		if(myIpLAN!=null){
+			Font.draw(screen, "Your ip LAN :"+myIpLAN, 100, 120);
+		}else{
+			searchIpLAN();
+		}
+		if(myIpWAN!=null){
+			Font.draw(screen, "Your ip WAN :"+myIpWAN, 100, 140);
+		}else{
+			searchIpWAN();
+		}
+		Font.draw(screen, "Waiting for client to join...", 100, 100);
+		
 		super.render(screen);
 	}
 
