cd01d9101e0dd0a0bc20848ffc62364cfeee78d5
Borsty <borstymail@googlemail.com>
2012-02-22 17:06:00 +0100

    Cleaned up commit :p

--- src/com/mojang/mojam/MojamComponent.java
+++ src/com/mojang/mojam/MojamComponent.java
@@ -44,7 +44,6 @@ import com.mojang.mojam.gui.JoinGameMenu;
 import com.mojang.mojam.gui.LevelSelect;
 import com.mojang.mojam.gui.PauseMenu;
 import com.mojang.mojam.gui.TitleMenu;
-import com.mojang.mojam.gui.WinMenu;
 import com.mojang.mojam.level.Level;
 import com.mojang.mojam.level.LevelInformation;
 import com.mojang.mojam.level.LevelList;
@@ -135,35 +134,44 @@ public class MojamComponent extends Canvas implements Runnable,
 		LevelList.createLevelList();
 	}
 
+	@Override
 	public void mouseDragged(MouseEvent arg0) {
 		mouseMoved = true;
 	}
 
+	@Override
 	public void mouseMoved(MouseEvent arg0) {
 		mouseMoved = true;
 	}
 
+	@Override
 	public void mouseClicked(MouseEvent e) {
 	}
 
+	@Override
 	public void mouseEntered(MouseEvent e) {
 	}
 
+	@Override
 	public void mouseExited(MouseEvent e) {
 		mouseButtons.releaseAll();
 	}
 
+	@Override
 	public void mousePressed(MouseEvent e) {
 		mouseButtons.setNextState(e.getButton(), true);
 	}
 
+	@Override
 	public void mouseReleased(MouseEvent e) {
 		mouseButtons.setNextState(e.getButton(), false);
 	}
 
+	@Override
 	public void paint(Graphics g) {
 	}
 
+	@Override
 	public void update(Graphics g) {
 	}
 
@@ -248,6 +256,7 @@ public class MojamComponent extends Canvas implements Runnable,
 		player.setCanSee(true);
 	}
 
+	@Override
 	public void run() {
 		long lastTime = System.nanoTime();
 		double unprocessed = 0;
@@ -399,12 +408,6 @@ public class MojamComponent extends Canvas implements Runnable,
 					* SCALE, null);
 		}
 
-		// String msg = "FPS: " + fps;
-		// g.setColor(Color.LIGHT_GRAY);
-		// g.drawString(msg, 11, 11);
-		// g.setColor(Color.WHITE);
-		// g.drawString(msg, 10, 10);
-
 	}
 
 	private void renderMouse(Screen screen, MouseButtons mouseButtons) {
@@ -432,47 +435,7 @@ public class MojamComponent extends Canvas implements Runnable,
 	}
 
 	private void tick() {
-//		if (level != null) {
-//			if (synchronizer.preTurn()) {
-//				synchronizer.postTurn();
-//
-//				if (!paused) {
-//					for (int index = 0; index < keys.getAll().size(); index++) {
-//						Keys.Key key = keys.getAll().get(index);
-//						boolean nextState = key.nextState;
-//						if (key.isDown != nextState) {
-//							synchronizer.addCommand(new ChangeKeyCommand(index,
-//									nextState));
-//						}
-//					}
-//
-//					keys.tick();
-//					for (Keys skeys : synchedKeys) {
-//						skeys.tick();
-//					}
-//
-//					//level.tick();
-//					if (keys.pause.wasPressed()) {
-//						keys.release();
-//						synchronizer.addCommand(new PauseCommand(true));
-//					}
-//
-//					if (level.player1Score >= Level.TARGET_SCORE) {
-//						addMenu(new WinMenu(GAME_WIDTH, GAME_HEIGHT, 1));
-//						soundPlayer.startEndMusic();
-//						level = null;
-//						return;
-//					}
-//					if (level.player2Score >= Level.TARGET_SCORE) {
-//						addMenu(new WinMenu(GAME_WIDTH, GAME_HEIGHT, 2));
-//						soundPlayer.startEndMusic();
-//						level = null;
-//						return;
-//					}
-//				}
-//			}
-//
-//		}
+
 		if (packetLink != null) {
 			packetLink.tick();
 		}
@@ -595,6 +558,7 @@ public class MojamComponent extends Canvas implements Runnable,
 
 	}
 
+	@Override
 	public void handle(int playerId, NetworkCommand packet) {
 
 		if (packet instanceof ChangeKeyCommand) {
@@ -614,6 +578,7 @@ public class MojamComponent extends Canvas implements Runnable,
 		}
 	}
 
+	@Override
 	public void handle(Packet packet) {
 		if (packet instanceof StartGamePacket) {
 			if (!isServer) {
@@ -634,6 +599,7 @@ public class MojamComponent extends Canvas implements Runnable,
 		}
 	}
 
+	@Override
 	public void buttonPressed(ClickableComponent component) {
 
 		if (component instanceof Button) {
@@ -681,6 +647,7 @@ public class MojamComponent extends Canvas implements Runnable,
 
 					hostThread = new Thread() {
 
+						@Override
 						public void run() {
 							boolean fail = true;
 							try {
@@ -772,18 +739,21 @@ public class MojamComponent extends Canvas implements Runnable,
 		}
 	}
 
+	@Override
 	public void keyPressed(KeyEvent e) {
 		if (!menuStack.isEmpty()) {
 			menuStack.peek().keyPressed(e);
 		}
 	}
 
+	@Override
 	public void keyReleased(KeyEvent e) {
 		if (!menuStack.isEmpty()) {
 			menuStack.peek().keyReleased(e);
 		}
 	}
 
+	@Override
 	public void keyTyped(KeyEvent e) {
 		if (!menuStack.isEmpty()) {
 			menuStack.peek().keyTyped(e);
