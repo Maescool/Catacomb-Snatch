3e9571bb7a94f3bd8599ec987ac2f0e2ab7e540a
Edmund Smith <eddysmundo@hotmail.co.uk>
2012-02-25 19:43:15 +0000

    Snatch Engine upload

---
+++ src/com/mojang/mojam/mod_testla.java
@@ -0,0 +1,29 @@
+package com.mojang.mojam;
+
+import com.mojang.mojam.network.Packet;
+
+public class mod_testla extends Mod
+{
+	public mod_testla()
+	{
+		System.out.println("Testla Running!");
+	}
+	
+	@Override
+	public void OnSendPacket(Packet p)
+	{
+		System.out.println(p.getId());
+	}
+	
+	@Override
+	public void RunOnce()
+	{
+		System.out.println("\"Rawr\" - Charles Testla, 1898");
+	}
+
+	@Override
+	public String getVersion()
+	{
+		return "Test-la!";
+	}
+}
