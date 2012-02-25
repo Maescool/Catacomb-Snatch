2f09f4182a19f34850b6a448263229521a5620db
Marlin Cremers <marlinc@mms-projects.net>
2012-02-25 23:15:57 +0100

    Some more changes

--- src/com/mojang/mojam/gui/CreditsScreen.java
+++ src/com/mojang/mojam/gui/CreditsScreen.java
@@ -1,6 +1,9 @@
 package com.mojang.mojam.gui;
 
 import java.awt.event.KeyEvent;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Vector;
 
 import com.mojang.mojam.MojamComponent;
 import com.mojang.mojam.Options;
@@ -12,29 +15,26 @@ public class CreditsScreen extends GuiMenu {
 	private final int gameHeight;
 
 	private ClickableComponent back;
-	public String text = "Project team\n"
-			+ "Leader: @Maescool\n"
-			+ "Original game: Mojang AB\n"
-			+ "Official developers: @Borsty, @danielduner, @flet, @judgedead53, @Maescool, @master-lincoln, @mkalam-alami, @Scorpion1122\n"
-			+ "Community management: @Austin01 (website manager, webmaster), @zorro300 (MCForums thread)\n"
-			+ "Other contributors:\nhttps://github.com/Maescool/Catacomb-Snatch/contributors";
+	
+	public String officialGame   = "Mojang AB";
+	public String[] leadDev      = {"@Maescool"};
+	public String[] officialDev  = {"@Borsty", "@danielduner", "@flet", "@judgedead53", "@Maescool",
+			"@master-lincoln", "@mkalam-alami", "@Scorpion1122"};
+	public String[] communityMan = {"@Austin01", "@zorro300"};
+	public String[] others = {"@xPaw", "@BubblegumBalloon", "@Elosanda", "@GreenLightning"};
 
 	public CreditsScreen(int gameWidth, int gameHeight) {
 		super();
 		this.gameWidth = gameWidth;
 		this.gameHeight = gameHeight;
 
-		addText(new Text(TitleMenu.CREDITS_TITLE_ID,
-				MojamComponent.texts.getStatic("options.credits"), (gameWidth - 512) / 2, 50));
-		addText(new Text(TitleMenu.CREDITS_TEXT_ID,
-				text, (gameWidth - 512) / 2, 80));
-		
 		back = addButton(new Button(TitleMenu.BACK_ID, MojamComponent.texts.getStatic("back"),
 				(gameWidth - 128) / 2, gameHeight - 50 - 30));
 	}
 
 	public void render(Screen screen) {
-
+		int previousY = 0;
+		
 		screen.clear(0);
 		screen.blit(Art.emptyBackground, 0, 0);
 
@@ -42,6 +42,59 @@ public class CreditsScreen extends GuiMenu {
 
 		screen.blit(Art.getLocalPlayerArt()[0][6], (gameWidth - 128) / 2 - 40,
 				gameHeight - 50 - 40);
+
+		screen.blit(Art.mojangLogo, (gameWidth - Art.mojangLogo.w) / 2, previousY += 20);
+		previousY += 20;
+		
+		addText(new Text(TitleMenu.CREDITS_TITLE_ID, MojamComponent.texts.getStatic("credits.leadDev"),
+			(gameWidth - 512) / 2, previousY += 20));
+		previousY = drawNames(leadDev, screen, previousY += 20);
+		addText(new Text(TitleMenu.CREDITS_TITLE_ID, MojamComponent.texts.getStatic("credits.maintainers"),
+			(gameWidth - 512) / 2, previousY += 20));
+		previousY = drawNames(officialDev, screen, previousY += 20);
+		addText(new Text(TitleMenu.CREDITS_TITLE_ID, MojamComponent.texts.getStatic("credits.communityMan"),
+			(gameWidth - 512) / 2, previousY += 20));
+		previousY = drawNames(communityMan, screen, previousY += 20);
+		addText(new Text(TitleMenu.CREDITS_TITLE_ID, MojamComponent.texts.getStatic("credits.others"),
+				(gameWidth - 512) / 2, previousY += 20));
+		previousY = drawNames(others, screen, previousY += 20);
+	}
+	
+	public int drawNames(String[] names, Screen screen, Integer y) {
+		List<Vector<String>> data = new Vector<Vector<String>>();
+		data.add(new Vector<String>());
+		data.add(new Vector<String>());
+		for (int i = 0; i < names.length; i++) {
+			String string = names[i];
+			if (i % 2 == 0) {
+				Vector<String> tmp = data.get(0);
+				tmp.add(string);
+				data.set(0, tmp);
+			} else {
+				Vector<String> tmp = data.get(1);
+				tmp.add(string);
+				data.set(1, tmp);
+			}
+		}
+		int groupId  = 0;
+		int drawY    = y;
+		int drawX    = 50;
+		int xOffset  = 200;
+		int yOffset  = 10;
+		int highestY = drawY;
+		for (Vector<String> group: data) {
+			drawX += xOffset * groupId;
+			for (String name : group) {
+				Font.draw(screen, name, drawX, drawY);
+				if (drawY > highestY) {
+					highestY = drawY;
+				}
+				drawY += yOffset;
+			}
+			drawY = y;
+			++groupId;
+		}
+		return highestY;
 	}
 
 	public void keyPressed(KeyEvent e) {
