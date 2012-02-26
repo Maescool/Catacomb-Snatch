095ccb108be7129ff79236ea823f9fbf3f4fa7b1
Philipp Abraham <abrahamp@informatik.uni-luebeck.de>
2012-02-26 15:52:12 +0100

    fix for joining MP

--- src/com/mojang/mojam/MojamComponent.java
+++ src/com/mojang/mojam/MojamComponent.java
@@ -866,7 +866,7 @@ public class MojamComponent extends Canvas implements Runnable,
 			isServer = false;
 			chat.clear();
 			
-			String[] data = TitleMenu.ip.split(":");
+			String[] data = TitleMenu.ip.trim().split(":");
 			String ip = data[0];
 			Integer port = (data.length > 0) ? Integer.parseInt(data[1]) : Options.getAsInteger(Options.MP_PORT, 3000);
 			
