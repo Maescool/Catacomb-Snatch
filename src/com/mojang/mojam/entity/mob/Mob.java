70735591af7d43afb528ddb602993645616f5397
Maik Macho <nuda1998@gmail.com>
2012-02-26 12:01:05 +0100

    Removed the mob health regeneration code because of an discussion

--- src/com/mojang/mojam/entity/mob/Mob.java
+++ src/com/mojang/mojam/entity/mob/Mob.java
@@ -40,8 +40,6 @@ public abstract class Mob extends Entity {
 	public boolean chasing=false;
 	public int justDroppedTicks = 0;
 	public int localTeam;
-	public static final int HEALING_INTERVAL = 15;
-	private int healingTime = HEALING_INTERVAL;
 	
 	public Mob(double x, double y, int team, int localTeam) {
 		super();
@@ -81,15 +79,6 @@ public abstract class Mob extends Entity {
 	}
 
 	public void tick() {
-		//Mob health regeneration - when not Nightmare mode ;)
-		if (hurtTime <= 0 && TitleMenu.difficulty.difficultyName != MojamComponent.texts.getStatic("diffselect.nightmare")) {
-			if (health < maxHealth) {
-				if (--healingTime <= 0) {
-					++health;
-					healingTime = HEALING_INTERVAL;
-				}
-			}
-		}
 		if (hurtTime > 0) {
 			hurtTime--;
 		}
@@ -205,7 +194,6 @@ public abstract class Mob extends Entity {
 	public void hurt(Entity source, float damage) {
 		if (isImmortal)
 			return;
-		healingTime = HEALING_INTERVAL;
 
 		if (freezeTime <= 0) {
 			
