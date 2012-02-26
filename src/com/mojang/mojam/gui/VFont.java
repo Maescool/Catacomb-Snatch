811500c2a09979619f4c94cfa267e8b9c5949d37
Mike T <fierydrake@no.spam>
2012-02-26 04:31:26 +0000

    Fixed a bug in VFont Should not have called getFont() in getFontStringWidth()

--- src/com/mojang/mojam/gui/VFont.java
+++ src/com/mojang/mojam/gui/VFont.java
@@ -5,7 +5,7 @@ import com.mojang.mojam.screen.Screen;
 
 public class VFont extends Font {
     public static String letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ    " + "abcdefghijklmnopqrstuvwxyz    " + "0123456789-.!?/%$\\=*+,;:()&#\"'";
-    private static final int pxFontHeight = 6;
+    private static final int GLYPH_HEIGHT = 6;
     private static final int SPACE = 1;
     
     protected VFont(Bitmap[][] bitmapData) {
@@ -20,7 +20,7 @@ public class VFont extends Font {
             int c = letters.indexOf(s.charAt(i));
             if (c < 0)
                 continue;
-            Bitmap image = getFont().bitmapData[c % 30][c / 30];
+            Bitmap image = bitmapData[c % 30][c / 30];
             w += image.w + SPACE;
         }
         return w;
@@ -28,7 +28,7 @@ public class VFont extends Font {
 
     @Override
     public int getFontStringHeight() {
-        return pxFontHeight;
+        return GLYPH_HEIGHT;
     }
     
     @Override
