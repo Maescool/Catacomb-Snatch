fe7d9b3a1739569d5ef2ba54860f2125da44d9d8
Sturmi <sturmflut@lieberbiber.de>
2012-02-26 04:19:01 +0100

    Refactor Player class: - The constructor is now the first method in the class - Move all class/member variables to the top - Add JavaDoc comments for every method - Delete an empty method

--- src/com/mojang/mojam/entity/building/Building.java
+++ src/com/mojang/mojam/entity/building/Building.java
@@ -14,15 +14,34 @@ import com.mojang.mojam.screen.Art;
 import com.mojang.mojam.screen.Bitmap;
 import com.mojang.mojam.screen.Screen;
 
+/**
+ * Generic building class
+ */
 public class Building extends Mob implements IUsable {
 	public static final int SPAWN_INTERVAL = 60;
 	public static final int MIN_BUILDING_DISTANCE = 1700; // Sqr
 	public static final int HEALING_INTERVAL = 15;
-	
+
 	public int spawnTime = 0;
 	public boolean highlight = false;
 	private int healingTime = HEALING_INTERVAL;
 
+	protected int upgradeLevel = 0;
+	private int maxUpgradeLevel = 0;
+	private int[] upgradeCosts = null;
+
+	/**
+	 * Constructor
+	 * 
+	 * @param x
+	 *            Initial X coordinate
+	 * @param y
+	 *            Initial Y coordinate
+	 * @param team
+	 *            Team number
+	 * @param localTeam
+	 *            Local team number
+	 */
 	public Building(double x, double y, int team, int localTeam) {
 		super(x, y, team, localTeam);
 
@@ -31,8 +50,6 @@ public class Building extends Mob implements IUsable {
 		spawnTime = TurnSynchronizer.synchedRandom.nextInt(SPAWN_INTERVAL);
 	}
 
-
-	
 	@Override
 	public void render(Screen screen) {
 		super.render(screen);
@@ -40,16 +57,25 @@ public class Building extends Mob implements IUsable {
 		renderInfo(screen);
 	}
 
+	/**
+	 * Render the marker onto the given screen
+	 * 
+	 * @param screen
+	 *            Screen
+	 */
 	protected void renderMarker(Screen screen) {
 		if (highlight) {
 			BB bb = getBB();
-			bb = bb.grow((getSprite().w - (bb.x1 - bb.x0)) / (3 + Math.sin(System.currentTimeMillis() * .01)));
+			bb = bb.grow((getSprite().w - (bb.x1 - bb.x0))
+					/ (3 + Math.sin(System.currentTimeMillis() * .01)));
 			int width = (int) (bb.x1 - bb.x0);
 			int height = (int) (bb.y1 - bb.y0);
 			Bitmap marker = new Bitmap(width, height);
 			for (int y = 0; y < height; y++) {
 				for (int x = 0; x < width; x++) {
-					if ((x < 2 || x > width - 3 || y < 2 || y > height - 3) && (x < 5 || x > width - 6) && (y < 5 || y > height - 6)) {
+					if ((x < 2 || x > width - 3 || y < 2 || y > height - 3)
+							&& (x < 5 || x > width - 6)
+							&& (y < 5 || y > height - 6)) {
 						int i = x + y * width;
 						marker.pixels[i] = 0xffffffff;
 					}
@@ -59,24 +85,38 @@ public class Building extends Mob implements IUsable {
 		}
 	}
 
-    protected void renderInfo(Screen screen) {
-    	//Draw iiAtlas' shop item info graphics
-        if (highlight) {
-        	Bitmap discriptionText = new Bitmap(110, 25);
-            BB bb = getBB();
-            
-            if(bb.x0 == 966.0) discriptionText = Art.turretText;
-            if(bb.x0 == 998.0) discriptionText = Art.harvesterText;
-            if(bb.x0 == 1030.0) discriptionText = Art.bombText;
-            
-            if(this.team == 1) {
-            	screen.blit(discriptionText, ((int) bb.x0 - (getSprite().w / 2)), ((int) bb.y0 + 30), 110, 25);  
-            }else if(this.team == 2) {
-            	screen.blit(discriptionText, ((int) bb.x1 - (getSprite().w / 2) - 30), ((int) bb.y1 - 60), 110, 25);
-            }
-        }
-    }
-	
+	/**
+	 * Render the shop info onto the given screen
+	 * 
+	 * @param screen
+	 *            Screen
+	 */
+	protected void renderInfo(Screen screen) {
+		// Draw iiAtlas' shop item info graphics
+		if (highlight) {
+			Bitmap discriptionText = new Bitmap(110, 25);
+			BB bb = getBB();
+
+			if (bb.x0 == 966.0)
+				discriptionText = Art.turretText;
+			if (bb.x0 == 998.0)
+				discriptionText = Art.harvesterText;
+			if (bb.x0 == 1030.0)
+				discriptionText = Art.bombText;
+
+			if (this.team == 1) {
+				screen.blit(discriptionText,
+						((int) bb.x0 - (getSprite().w / 2)),
+						((int) bb.y0 + 30), 110, 25);
+			} else if (this.team == 2) {
+				screen.blit(discriptionText,
+						((int) bb.x1 - (getSprite().w / 2) - 30),
+						((int) bb.y1 - 60), 110, 25);
+			}
+		}
+	}
+
+	@Override
 	public void tick() {
 		super.tick();
 		if (freezeTime > 0) {
@@ -107,84 +147,91 @@ public class Building extends Mob implements IUsable {
 		healingTime = HEALING_INTERVAL;
 	}
 
+	@Override
 	public Bitmap getSprite() {
 		return Art.floorTiles[3][2];
 	}
 
+	@Override
 	public boolean move(double xBump, double yBump) {
 		return false;
 	}
 
+	@Override
 	public void slideMove(double xa, double ya) {
 		super.move(xa, ya);
 		fallDownHole();
 	}
 
-	//
-	// Upgrade
-	//
-	protected void upgradeComplete() {
-
-	}
-
-	protected int upgradeLevel = 0;
-	private int maxUpgradeLevel = 0;
-	private int[] upgradeCosts = null;
-
+	@Override
 	public boolean upgrade(Player p) {
 		if (upgradeLevel >= maxUpgradeLevel) {
-			MojamComponent.soundPlayer.playSound("/sound/Fail.wav", (float) pos.x, (float) pos.y, true);
-			if(this.team == this.localTeam) {
-				Notifications.getInstance().add(MojamComponent.texts.getStatic("upgrade.full"));
+			MojamComponent.soundPlayer.playSound("/sound/Fail.wav",
+					(float) pos.x, (float) pos.y, true);
+			if (this.team == this.localTeam) {
+				Notifications.getInstance().add(
+						MojamComponent.texts.getStatic("upgrade.full"));
 			}
 			return false;
 		}
 
 		final int cost = upgradeCosts[upgradeLevel];
 		if (cost > p.getScore() && !Options.getAsBoolean(Options.CREATIVE)) {
-			MojamComponent.soundPlayer.playSound("/sound/Fail.wav", (float) pos.x, (float) pos.y, true);
-			if(this.team == this.localTeam) {
-				Notifications.getInstance().add(MojamComponent.texts.upgradeNotEnoughMoney(cost));
+			MojamComponent.soundPlayer.playSound("/sound/Fail.wav",
+					(float) pos.x, (float) pos.y, true);
+			if (this.team == this.localTeam) {
+				Notifications.getInstance().add(
+						MojamComponent.texts.upgradeNotEnoughMoney(cost));
 			}
 			return false;
 		}
 
-		MojamComponent.soundPlayer.playSound("/sound/Upgrade.wav", (float) pos.x, (float) pos.y, true);
+		MojamComponent.soundPlayer.playSound("/sound/Upgrade.wav",
+				(float) pos.x, (float) pos.y, true);
 
 		++upgradeLevel;
 		p.useMoney(cost);
-		upgradeComplete();
-		
-		if(this.team == this.localTeam) {
-			Notifications.getInstance().add(MojamComponent.texts.upgradeTo(upgradeLevel+1));
+
+		if (this.team == this.localTeam) {
+			Notifications.getInstance().add(
+					MojamComponent.texts.upgradeTo(upgradeLevel + 1));
 		}
 		return true;
 	}
 
+	/**
+	 * Make the building upgradeable
+	 * 
+	 * @param costs Cost vector
+	 */
 	void makeUpgradeableWithCosts(int[] costs) {
-		maxUpgradeLevel = 0;
-		if (costs == null)
-			return;
-
-		upgradeCosts = costs;
-		maxUpgradeLevel = costs.length - 1;
-		upgradeComplete();
+		if (costs == null) {
+			maxUpgradeLevel = 0;
+		} else {
+			upgradeCosts = costs;
+			maxUpgradeLevel = costs.length - 1;
+		}
+		return;
 	}
 
+	@Override
 	public void use(Entity user) {
 		if (user instanceof Player) {
 			((Player) user).pickup(this);
 		}
 	}
-  
+
+	@Override
 	public boolean isHighlightable() {
 		return true;
 	}
 
+	@Override
 	public void setHighlighted(boolean hl) {
 		highlight = hl;
 	}
 
+	@Override
 	public boolean isAllowedToCancel() {
 		return true;
 	}
