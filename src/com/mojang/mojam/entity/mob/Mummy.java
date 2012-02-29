4879178d1cd23a6303028199406c6e3c05c06a28
RaXaR <raxar@meccanine.com>
2012-02-29 21:59:25 +0200

    Reverted Mummy-Shadows until PNG transparency is supported.

--- src/com/mojang/mojam/entity/mob/Mummy.java
+++ src/com/mojang/mojam/entity/mob/Mummy.java
@@ -4,17 +4,12 @@ import com.mojang.mojam.entity.Player;
 import com.mojang.mojam.network.TurnSynchronizer;
 import com.mojang.mojam.screen.Art;
 import com.mojang.mojam.screen.Bitmap;
-import com.mojang.mojam.screen.Screen;
 
 public class Mummy extends HostileMob {
 
     private int tick = 0;
     public static double ATTACK_RADIUS = 128.0;
 
-    private int spriteIdx;
-    private int facingIdx;
-    private Bitmap shadow;
-    
     public Mummy(double x, double y) {
         super(x, y, Team.Neutral);
         setPos(x, y);
@@ -42,48 +37,16 @@ public class Mummy extends HostileMob {
         walk();
     }
 
-    @Override
     public void die() {
         super.die();
     }
 
-    @Override
     public Bitmap getSprite() {
-        spriteIdx = ((stepTime / 6) & 3);
-        facingIdx = (facing + 1) & 3;
-        return Art.mummy[spriteIdx][facingIdx];
+        return Art.mummy[((stepTime / 6) & 3)][(facing + 1) & 3];
     }
 
     @Override
     public String getDeathSound() {
         return "/sound/Enemy Death 2.wav";
     }
-    
-    @Override
-	public void render(Screen screen) {
-		Bitmap image = getSprite();
-        
-        // Render shadow
-        shadow = Art.mummyShadow[spriteIdx][facingIdx];
-        screen.opacityBlit(shadow, (int)(pos.x - shadow.w / 2), (int)(pos.y - shadow.h / 2 - 10), 0x99);
-        
-		if (hurtTime > 0) {
-			if (hurtTime > 40 - 6 && hurtTime / 2 % 2 == 0) {
-				screen.colorBlit(image, pos.x - image.w / 2, pos.y - image.h / 2 - yOffs, 0xa0ffffff);
-			} else {
-				if (health < 0)
-					health = 0;
-				int col = (int) (180 - health * 180 / maxHealth);
-				if (hurtTime < 10)
-					col = col * hurtTime / 10;
-				screen.colorBlit(image, pos.x - image.w / 2, pos.y - image.h / 2 - yOffs, (col << 24) + 255 * 65536);
-			}
-		} else {
-			screen.blit(image, pos.x - image.w / 2, pos.y - image.h / 2 - yOffs);
-		}
-
-		if (doShowHealthBar && health < maxHealth) {
-            addHealthBar(screen);
-        }
-	}    
 }
