f736631b3275474411dea696990ed8896ff883df
Mike T <fierydrake@no.spam>
2012-03-03 04:28:44 +0000

    Made ShopItem a public class to fix compile error Used by Player class to check if a building is a shop
    Resolved compile error when merging latest upstream
    changes.
    
    Also, removed unused import in Building while I was
    passing.

--- src/com/mojang/mojam/entity/building/Building.java
+++ src/com/mojang/mojam/entity/building/Building.java
@@ -6,7 +6,6 @@ import com.mojang.mojam.entity.Entity;
 import com.mojang.mojam.entity.IUsable;
 import com.mojang.mojam.entity.Player;
 import com.mojang.mojam.entity.mob.Mob;
-import com.mojang.mojam.gui.Font;
 import com.mojang.mojam.gui.Notifications;
 import com.mojang.mojam.math.BB;
 import com.mojang.mojam.network.TurnSynchronizer;
