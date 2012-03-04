d939992e2452581015ea8733e5f5860acf663136
Pieter Maes <maescool@gmail.com>
2012-03-04 20:12:18 +0100

    Poison should not harm buildings

--- src/com/mojang/mojam/entity/BulletPoison.java
+++ src/com/mojang/mojam/entity/BulletPoison.java
@@ -1,5 +1,7 @@
 package com.mojang.mojam.entity;
 
+import com.mojang.mojam.MojamComponent;
+import com.mojang.mojam.entity.building.Building;
 import com.mojang.mojam.entity.mob.Mob;
 import com.mojang.mojam.screen.Art;
 import com.mojang.mojam.screen.Screen;
@@ -19,5 +21,18 @@ public class BulletPoison extends Bullet {
 		screen.blit(Art.bulletpoison[facing][0], pos.x - 8, pos.y - 10);
 	}
 	
+	@Override
+	public void collide(Entity entity, double xa, double ya) {
+		if (entity instanceof Mob) {
+			Mob mobEnt = (Mob) entity;
+			if (mobEnt.isNotFriendOf(owner) && !(entity instanceof Building)) {
+				mobEnt.hurt(this,damage);
+				hit = true;
+			}
+		}
+		if (hit) {
+			MojamComponent.soundPlayer.playSound("/sound/Shot 2.wav", (float) pos.x, (float) pos.y);
+		}
+	}
 	
 }
