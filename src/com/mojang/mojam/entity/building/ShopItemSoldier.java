29d234ced3cffc6fd5e06a2d20acb409b7d103f9
Morgan Gilroy <morgan.gilroy@gmail.com>
2012-03-13 02:25:58 +0000

    Added player level check and list of Soldiers to keep track of how many have been spawned. also added Gray out when too may soldiers are spawned.

--- src/com/mojang/mojam/entity/building/ShopItemSoldier.java
+++ src/com/mojang/mojam/entity/building/ShopItemSoldier.java
@@ -1,29 +1,60 @@
 package com.mojang.mojam.entity.building;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.mojang.mojam.GameCharacter;
+import com.mojang.mojam.MojamComponent;
 import com.mojang.mojam.entity.Player;
 import com.mojang.mojam.entity.mob.Team;
 import com.mojang.mojam.entity.mob.pather.Soldier;
 import com.mojang.mojam.screen.Art;
+import com.mojang.mojam.screen.Bitmap;
 
 public class ShopItemSoldier extends ShopItem {
 
+	Bitmap baseSprite;
+	List<Soldier> soldiers=new ArrayList<Soldier>();
+	
 	public ShopItemSoldier(double x, double y, int team) {
 		super("soldier", x, y, team, 1000, 8);
 
 		int facing = (team == 2) ? 0 : 4;
 
 		if (team == Team.Team1) {
-			setSprite(Art.getPlayer(GameCharacter.LordLard)[facing][0]);
+			baseSprite=Art.getPlayer(GameCharacter.LordLard)[facing][0].copy();
 		} else {
-			setSprite(Art.getPlayer(GameCharacter.HerrVonSpeck)[facing][0]);
+			baseSprite=Art.getPlayer(GameCharacter.HerrVonSpeck)[facing][0].copy();
 		}
+		setSprite(baseSprite);
 	}
 
 	public void useAction(Player player) {
-		if (player.getTeam() == team) {
-			level.addEntity(new Soldier(player.pos.x, player.pos.y, team,
-					player));
+		if (player.getTeam() == team && checkPlayerLevel(player)) {
+			Soldier soldier = new Soldier(player.pos.x, player.pos.y, team,	player);
+			soldiers.add(soldier);
+			level.addEntity(soldier);
 		}
+	}
+	
+	public void tick() {
+		//check if player is the right level to buy stuff!
+		//checkPlayerLevel();
+		//checkSoldierList();
+		
+		if (!( this.team == MojamComponent.localTeam && checkPlayerLevel(MojamComponent.localPlayer))) {
+			Bitmap sprite=new Bitmap(baseSprite.w,baseSprite.h);
+			int col=64+(int)(64*(Math.sin(System.currentTimeMillis() * .001)+1.5));
+			sprite.colorBlit(baseSprite, 0, 0, (0xAA<<24)|(col)|(col<<8)|(col<<16));
+			setSprite(sprite);
+		} else {
+			setSprite(baseSprite);
+		}
+	}
+	
+	protected boolean checkPlayerLevel(Player player) {
+		if ( soldiers.size() > player.getPlevel())
+			return false;
+		return true;
 	}
 }
