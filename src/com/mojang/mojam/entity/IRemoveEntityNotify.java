f20fe19324532db61a1dbb2bceab73a3b64fdefa
Morgan Gilroy <morgan.gilroy@gmail.com>
2012-03-15 21:30:59 +0000

    Added java doc to IRemoveEntityNotify

--- src/com/mojang/mojam/entity/IRemoveEntityNotify.java
+++ src/com/mojang/mojam/entity/IRemoveEntityNotify.java
@@ -1,5 +1,15 @@
 package com.mojang.mojam.entity;
 
+/**
+ * Simple interface used as a cut down Observer type call back function
+ * so when an entity dies it can notify something else.
+ * 
+ * currently use in Soldier and ShopItemSoldier
+ * 
+ * @see Soldier
+ * @see ShopItemSoldier
+ * @author Morgan
+ */
 public interface IRemoveEntityNotify {
 
 	public void removeEntityNotice(Entity e);
