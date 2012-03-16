d48aa4472a32c85e8f9cb8c6d05f5e95efddeacc
Morgan Gilroy <morgan.gilroy@gmail.com>
2012-03-16 00:42:32 +0000

    added initial ICarrySwap

---
+++ src/com/mojang/mojam/entity/ICarrySwap.java
@@ -0,0 +1,27 @@
+package com.mojang.mojam.entity;
+
+import com.mojang.mojam.entity.building.Building;
+
+/**
+ * Interface defines if
+ * 1. it can carry a Building
+ * 2. if it can, can it swap with another entity
+ * 
+ * @author Morgan
+ *
+ */
+public interface ICarrySwap {
+
+	public boolean isCarrying();
+	
+	public boolean canCarry(Building b);
+	
+	public boolean canPickup(Building b);
+	
+	public Building getCarrying(Entity e);
+		
+	public void pickup(Building b);
+	
+	public Building tryToSwap(Building b);	
+	
+}
