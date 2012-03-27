474925ff4a9bcf17b3e521940351846b83bf5009
Edmund Smith <eddysmundo@hotmail.co.uk>
2012-03-27 23:17:45 +0100

    Added empty template mods

---
+++ src/com/mojang/mojam/mod/mod_.py
@@ -0,0 +1,39 @@
+from java import com.mojang.mojam.mods.ModSystem;
+
+def OnRender():
+
+
+def OnLevelTick(level):
+
+
+def OnTick():
+
+    
+def AfterTick():
+
+    
+def OnStartRender():
+
+    
+def RunOnce():
+
+    
+def OnClose():
+
+    
+def OnSendPacket(packet):
+
+    
+def OnStop():
+
+    
+def OnVictory(team):
+
+    
+def OnReceivePacket(packet):
+
+    
+def HandlePacket(packet):
+
+    
+def CreateLevel(level):
---
+++ src/com/mojang/mojam/mod/mod_.rb
@@ -0,0 +1,25 @@
+function OnRender(){}
+
+function OnLevelTick(level){}
+
+function OnTick(){}
+    
+function AfterTick(){}
+    
+function OnStartRender(){}
+    
+function RunOnce(){}
+    
+function OnClose(){}
+    
+function OnSendPacket(packet){}
+    
+function OnStop(){}
+    
+function OnVictory(team){}
+    
+function OnReceivePacket(packet){}
+    
+function HandlePacket(packet){}
+    
+function CreateLevel(level){}
